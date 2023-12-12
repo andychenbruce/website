701dd6d64612ae56a8673907f66dae4e5ad8a884
陈功 <chengong456@qq.com>
2023-12-12 14:44:31 -0800

    stuff

--- crates/wasm_test/src/lib.rs
+++ crates/wasm_test/src/lib.rs
@@ -7,7 +7,7 @@ pub async fn andy_main() -> Result<(), JsValue> {
 }
 
 fn setup_canvas() {
-    let canvas: web_sys::WebGlRenderingContext = web_sys::window()
+    let context: web_sys::WebGl2RenderingContext = web_sys::window()
         .unwrap()
         .document()
         .unwrap()
@@ -15,13 +15,96 @@ fn setup_canvas() {
         .unwrap()
         .dyn_into::<web_sys::HtmlCanvasElement>()
         .unwrap()
-        .get_context("webgl")
+        .get_context("webgl2")
         .unwrap()
         .unwrap()
         .dyn_into()
         .unwrap();
-    canvas.clear_color(0.1, 0.1, 0.9, 1.0);
-    canvas.clear(web_sys::WebGlRenderingContext::COLOR_BUFFER_BIT);
+    context.clear_color(0.1, 0.1, 0.9, 1.0);
+    context.clear(web_sys::WebGl2RenderingContext::COLOR_BUFFER_BIT);
+
+    let vert_shader_source = include_str!("./shaders_source/vertex.glsl");
+    let frag_shader_source = include_str!("./shaders_source/frag.glsl");
+
+    let program = make_program(&context, vert_shader_source, frag_shader_source);
+
+    context.use_program(Some(&program));
+
+    let buffer = context.create_buffer().unwrap();
+    context.bind_buffer(web_sys::WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
+    let vertices: &[f32] = &[-0.2, 0.5, 0.0, -0.5, -0.4, 0.0, 0.5, -0.1, 0.0];
+
+    let positions_array_buf_view = unsafe { js_sys::Float32Array::view(vertices) };
+    context.buffer_data_with_array_buffer_view(
+        web_sys::WebGl2RenderingContext::ARRAY_BUFFER,
+        &positions_array_buf_view,
+        web_sys::WebGl2RenderingContext::STATIC_DRAW,
+    );
+
+    let vao = context.create_vertex_array().unwrap();
+    context.bind_vertex_array(Some(&vao));
+
+    let position_attrib = context.get_attrib_location(&program, "position");
+    context.vertex_attrib_pointer_with_i32(
+        position_attrib as u32,
+        3,
+        web_sys::WebGl2RenderingContext::FLOAT,
+        false,
+        0,
+        0,
+    );
+    context.enable_vertex_attrib_array(position_attrib as u32);
+
+    context.bind_vertex_array(Some(&vao));
+
+    let vert_count = (vertices.len() / 3) as i32;
+    context.clear(web_sys::WebGl2RenderingContext::COLOR_BUFFER_BIT);
+
+    context.draw_arrays(web_sys::WebGl2RenderingContext::TRIANGLES, 0, vert_count);
+}
+
+fn make_program(
+    context: &web_sys::WebGl2RenderingContext,
+    vert_shader_code: &str,
+    frag_shader_code: &str,
+) -> web_sys::WebGlProgram {
+    fn make_shader(
+        context: &web_sys::WebGl2RenderingContext,
+        shader_type: u32,
+        source: &str,
+    ) -> web_sys::WebGlShader {
+        let shader = context.create_shader(shader_type).unwrap();
+
+        context.shader_source(&shader, source);
+        context.compile_shader(&shader);
+        if context
+            .get_shader_parameter(&shader, web_sys::WebGl2RenderingContext::COMPILE_STATUS)
+            .as_bool()
+            .unwrap()
+        {
+            shader
+        } else {
+            panic!("compile shader failed");
+        }
+    }
+
+    let vert_shader = make_shader(
+        context,
+        web_sys::WebGl2RenderingContext::VERTEX_SHADER,
+        vert_shader_code,
+    );
+
+    let frag_shader = make_shader(
+        context,
+        web_sys::WebGl2RenderingContext::FRAGMENT_SHADER,
+        frag_shader_code,
+    );
+
+    let program = context.create_program().unwrap();
+    context.attach_shader(&program, &vert_shader);
+    context.attach_shader(&program, &frag_shader);
+    context.link_program(&program);
+    program
 }
 
 #[wasm_bindgen]
