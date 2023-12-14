9d8038b157cf1f374c4482b45dae7bc1e145efcb
陈功 <chengong456@qq.com>
2023-12-13 16:32:18 -0800

    event loop

--- crates/wasm_test/src/lib.rs
+++ crates/wasm_test/src/lib.rs
@@ -3,10 +3,14 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 pub async fn andy_main() -> Result<(), JsValue> {
     web_sys::console::log_1(&"hi lol\n".into());
+    setup_canvas().await;
     Ok(())
 }
+thread_local!(
+    static LAST_TICK_TIME: std::cell::RefCell<f64> = std::cell::RefCell::new(0.0);
+);
 
-fn setup_canvas() {
+async fn setup_canvas() {
     let context: web_sys::WebGl2RenderingContext = web_sys::window()
         .unwrap()
         .document()
@@ -27,7 +31,6 @@ fn setup_canvas() {
     let frag_shader_source = include_str!("./shaders_source/frag.glsl");
 
     let program = make_program(&context, vert_shader_source, frag_shader_source);
-
     context.use_program(Some(&program));
 
     let buffer = context.create_buffer().unwrap();
@@ -58,9 +61,34 @@ fn setup_canvas() {
     context.bind_vertex_array(Some(&vao));
 
     let vert_count = (vertices.len() / 3) as i32;
-    context.clear(web_sys::WebGl2RenderingContext::COLOR_BUFFER_BIT);
 
+    let f = std::rc::Rc::new(std::cell::RefCell::new(None));
+    let g = f.clone();
+
+    *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+        draw(&context, vert_count);
+        request_animation_frame(f.borrow().as_ref().unwrap());
+    }) as Box<dyn FnMut()>));
+
+    request_animation_frame(g.borrow().as_ref().unwrap());
+}
+
+fn draw(context: &web_sys::WebGl2RenderingContext, vert_count: i32) {
+    context.clear(web_sys::WebGl2RenderingContext::COLOR_BUFFER_BIT);
     context.draw_arrays(web_sys::WebGl2RenderingContext::TRIANGLES, 0, vert_count);
+    let current_time_milis = js_sys::Date::new_0().get_time();
+    LAST_TICK_TIME.with(|f| {
+        let diff_milis = current_time_milis - *f.borrow();
+        web_sys::console::log_1(&(format!("drew stuff diff: {}", diff_milis)).into());
+        *f.borrow_mut() = current_time_milis
+    });
+}
+
+fn request_animation_frame(f: &Closure<dyn FnMut()>) -> i32 {
+    web_sys::window()
+        .unwrap()
+        .request_animation_frame(f.as_ref().dyn_ref().unwrap())
+        .unwrap()
 }
 
 fn make_program(
@@ -68,8 +96,6 @@ fn make_program(
     vert_shader_code: &str,
     frag_shader_code: &str,
 ) -> web_sys::WebGlProgram {
-    
-
     let vert_shader = make_shader(
         context,
         web_sys::WebGl2RenderingContext::VERTEX_SHADER,
@@ -102,23 +128,21 @@ fn make_shader(
         .get_shader_parameter(&shader, web_sys::WebGl2RenderingContext::COMPILE_STATUS)
         .as_bool()
         .unwrap()
-        {
-            shader
-        } else {
-            web_sys::console::error_1(
-                &context
-                    .get_shader_info_log(&shader)
-                    .unwrap()
-                    .as_str()
-                    .into(),
-            );
-            panic!("compiling shader failed")
-        }
+    {
+        shader
+    } else {
+        web_sys::console::error_1(
+            &context
+                .get_shader_info_log(&shader)
+                .unwrap()
+                .as_str()
+                .into(),
+        );
+        panic!("compiling shader failed")
+    }
 }
 
 #[wasm_bindgen]
 pub fn set_panic_hook() {
     console_error_panic_hook::set_once();
-    setup_canvas();
 }
-
