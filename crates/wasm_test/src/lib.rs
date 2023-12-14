cd882d2492fbe78f16895855bc706ca0916790f5
陈功 <chengong456@qq.com>
2023-12-13 23:00:48 -0800

    sphere start

--- crates/wasm_test/src/lib.rs
+++ crates/wasm_test/src/lib.rs
@@ -1,8 +1,19 @@
 use wasm_bindgen::prelude::*;
 
+const PHI: f32 = 1.618_034;
+const GOLDEN_RECTANGLE: &[f32] = &[
+    -1.0, PHI, 0.0, 1.0, PHI, 0.0, 1.0, -PHI, 0.0, -1.0, PHI, 0.0, -1.0, -PHI, 0.0, 1.0, -PHI, 0.0,
+];
+
+#[repr(u32)]
+enum FragEnum {
+    Red = 0,
+    Green = 1,
+    Blue = 2,
+}
+
 #[wasm_bindgen]
 pub async fn andy_main() -> Result<(), JsValue> {
-    web_sys::console::log_1(&"hi lol\n".into());
     setup_canvas().await;
     Ok(())
 }
@@ -11,6 +22,7 @@ struct Globals {
     last_tick_time: u64,
     context: web_sys::WebGl2RenderingContext,
     model_matrix_location: web_sys::WebGlUniformLocation,
+    frag_enum_location: web_sys::WebGlUniformLocation,
 }
 
 async fn setup_canvas() {
@@ -27,8 +39,9 @@ async fn setup_canvas() {
         .unwrap()
         .dyn_into()
         .unwrap();
-    context.clear_color(0.1, 0.1, 0.9, 1.0);
+    context.clear_color(0.7, 0.7, 0.5, 1.0);
     context.clear(web_sys::WebGl2RenderingContext::COLOR_BUFFER_BIT);
+    context.enable(web_sys::WebGl2RenderingContext::DEPTH_TEST);
 
     let vert_shader_source = include_str!("./shaders_source/vertex.glsl");
     let frag_shader_source = include_str!("./shaders_source/frag.glsl");
@@ -38,9 +51,8 @@ async fn setup_canvas() {
 
     let buffer = context.create_buffer().unwrap();
     context.bind_buffer(web_sys::WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
-    let vertices: &[f32] = &[-0.5, -0.4, 0.0, 0.5, -0.1, 0.0, -0.2, 0.5, 0.0];
 
-    let positions_array_buf_view = unsafe { js_sys::Float32Array::view(vertices) };
+    let positions_array_buf_view = unsafe { js_sys::Float32Array::view(GOLDEN_RECTANGLE) };
     context.buffer_data_with_array_buffer_view(
         web_sys::WebGl2RenderingContext::ARRAY_BUFFER,
         &positions_array_buf_view,
@@ -51,7 +63,7 @@ async fn setup_canvas() {
         fovy: cgmath::Rad(2.0),
         aspect: 1.0,
         near: 0.001,
-        far: 3.0,
+        far: 10.0,
     });
 
     let perspective_matrix_location = context
@@ -65,6 +77,7 @@ async fn setup_canvas() {
     let model_matrix_location = context
         .get_uniform_location(&program, "modelMatrix")
         .unwrap();
+    let frag_enum_location = context.get_uniform_location(&program, "fragEnum").unwrap();
 
     let vao = context.create_vertex_array().unwrap();
     context.bind_vertex_array(Some(&vao));
@@ -80,54 +93,83 @@ async fn setup_canvas() {
     );
     context.enable_vertex_attrib_array(position_attrib as u32);
 
-    let vert_count = (vertices.len() / 3) as i32;
-
     let mut globals = Globals {
         last_tick_time: 0,
         context,
         model_matrix_location,
+        frag_enum_location,
     };
 
     let f = std::rc::Rc::new(std::cell::RefCell::new(None));
     let g = f.clone();
 
     *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
-        draw(&mut globals, vert_count);
+        draw(&mut globals);
         request_animation_frame(f.borrow().as_ref().unwrap());
     }) as Box<dyn FnMut()>));
 
     request_animation_frame(g.borrow().as_ref().unwrap());
 }
 
-fn draw(globals: &mut Globals, vert_count: i32) {
+fn draw(globals: &mut Globals) {
+    globals
+        .context
+        .clear(web_sys::WebGl2RenderingContext::COLOR_BUFFER_BIT);
+
     let current_time_milis = js_sys::Date::new_0().get_time() as u64;
     globals.last_tick_time = current_time_milis;
-    let rotate_vector = cgmath::Vector3 {
-        x: 0.0,
-        y: 1.0,
-        z: 1.0,
-    };
     let divisor: f64 = 1000.0;
     let radians =
         ((current_time_milis as f64).rem_euclid(std::f64::consts::TAU * divisor) / divisor) as f32;
 
-    let model_matrix: cgmath::Matrix4<f32> =
+    let rotate_vector = cgmath::Vector3 {
+        x: 0.0,
+        y: 1.0,
+        z: 0.0,
+    };
+
+    let forward_rotated =
         cgmath::Matrix4::from_translation(cgmath::Vector3 {
             x: 0.0,
-            y: 0.0,
-            z: -0.5,
+            y: -0.5,
+            z: -3.0,
         }) * cgmath::Matrix4::from_axis_angle(normalize(rotate_vector), cgmath::Rad(radians));
-    globals.context.uniform_matrix4fv_with_f32_array(
-        Some(&globals.model_matrix_location),
-        false,
-        &matrix_to_vec(model_matrix),
+
+    draw_rectangle(globals, forward_rotated, FragEnum::Red);
+    let rotated_1 = cgmath::Matrix4::from_axis_angle(
+        normalize(cgmath::Vector3 {
+            x: 1.0,
+            y: 0.0,
+            z: 1.0,
+        }),
+        cgmath::Rad(std::f32::consts::TAU / 2.0),
+    ) * cgmath::Matrix4::from_axis_angle(
+        normalize(cgmath::Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: 1.0,
+        }),
+        cgmath::Rad(std::f32::consts::TAU / 4.0),
     );
-    globals
-        .context
-        .clear(web_sys::WebGl2RenderingContext::COLOR_BUFFER_BIT);
-    globals
-        .context
-        .draw_arrays(web_sys::WebGl2RenderingContext::TRIANGLES, 0, vert_count);
+    draw_rectangle(globals, forward_rotated * rotated_1, FragEnum::Green);
+
+    draw_rectangle(globals, forward_rotated, FragEnum::Red);
+    let rotated_2 = cgmath::Matrix4::from_axis_angle(
+        normalize(cgmath::Vector3 {
+            x: 1.0,
+            y: 1.0,
+            z: 0.0,
+        }),
+        cgmath::Rad(std::f32::consts::TAU / 2.0),
+    ) * cgmath::Matrix4::from_axis_angle(
+        normalize(cgmath::Vector3 {
+            x: 0.0,
+            y: 1.0,
+            z: 0.0,
+        }),
+        cgmath::Rad(std::f32::consts::TAU / 4.0),
+    );
+    draw_rectangle(globals, forward_rotated * rotated_2, FragEnum::Blue);
 }
 
 fn request_animation_frame(f: &Closure<dyn FnMut()>) -> i32 {
@@ -170,13 +212,12 @@ fn make_shader(
 
     context.shader_source(&shader, source);
     context.compile_shader(&shader);
-    web_sys::console::warn_1(
-        &context
-            .get_shader_info_log(&shader)
-            .unwrap()
-            .as_str()
-            .into(),
-    );
+
+    let shader_log = context.get_shader_info_log(&shader).unwrap();
+
+    if !shader_log.is_empty() {
+        web_sys::console::warn_1(&format!("shader compilation has errors: {}", shader_log).into());
+    }
     if context
         .get_shader_parameter(&shader, web_sys::WebGl2RenderingContext::COMPILE_STATUS)
         .as_bool()
@@ -206,6 +247,21 @@ fn normalize(vec: cgmath::Vector3<f32>) -> cgmath::Vector3<f32> {
     let len_squared: f32 = (vec.x * vec.x) + (vec.y * vec.y) + (vec.z * vec.z);
 
     vec / (len_squared.sqrt())
+}
+fn draw_rectangle(globals: &Globals, model_mat: cgmath::Matrix4<f32>, frag_enum: FragEnum) {
+    globals.context.uniform_matrix4fv_with_f32_array(
+        Some(&globals.model_matrix_location),
+        false,
+        &matrix_to_vec(model_mat),
+    );
+    globals
+        .context
+        .uniform1ui(Some(&globals.frag_enum_location), frag_enum as u32);
+    globals.context.draw_arrays(
+        web_sys::WebGl2RenderingContext::TRIANGLES,
+        0,
+        (GOLDEN_RECTANGLE.len() / 3) as i32,
+    );
 }
 
 #[wasm_bindgen]
--- crates/wasm_test/src/shaders_source/frag.glsl
+++ crates/wasm_test/src/shaders_source/frag.glsl
@@ -2,10 +2,23 @@
 
 precision highp float;
 
-in vec4 input_pos;
+uniform uint fragEnum;
+uint ENUM_RED = 0u;
+uint ENUM_GREEN = 1u;
+uint ENUM_BLUE = 2u;
 
+
+in vec4 input_pos;
 out vec4 outColor;
 
 void main() {
-     outColor = input_pos;
+  if(fragEnum == ENUM_RED){
+    outColor = vec4(1.0, 0.0, 0.0, 1.0);
+  }else if(fragEnum == ENUM_GREEN){
+    outColor = vec4(0.0, 1.0, 0.0, 1.0);
+  }else if(fragEnum == ENUM_BLUE){
+    outColor = vec4(0.0, 0.0, 1.0, 1.0);
+  }else{
+    outColor = input_pos;
+  }
 }
