6cf4b8787cae292ee41466f54ec9a5926c6ed239
陈功 <chengong456@qq.com>
2023-12-17 22:15:53 -0800

    icosahedron done

--- crates/icosahedron/src/lib.rs
+++ crates/icosahedron/src/lib.rs
@@ -7,7 +7,6 @@ const PHI: f32 = 1.618_034;
 const GOLDEN_RECTANGLE_VERTS: &[f32] = &[
     -1.0, PHI, 0.0, 1.0, PHI, 0.0, 1.0, -PHI, 0.0, -1.0, PHI, 0.0, -1.0, -PHI, 0.0, 1.0, -PHI, 0.0,
 ];
-
 #[repr(u32)]
 #[derive(Copy, Clone)]
 #[allow(dead_code)]
@@ -16,9 +15,13 @@ enum FragEnum {
     Green = 1,
     Blue = 2,
     Black = 3,
-    ClearRed = 4,
-    ClearGreen = 5,
-    ClearBlue = 6,
+    White = 4,
+    ClearRed = 5,
+    ClearGreen = 6,
+    ClearBlue = 7,
+    RedReflective = 8,
+    RedSpectral = 9,
+    BeachBall = 10,
 }
 
 struct MouseState {
@@ -32,13 +35,15 @@ enum FragLevel {
     None,
     Transparent,
     Full,
-    //BeachBall,
-    //Refractive
+    Reflective,
+    Spectral,
+    BeachBall,
 }
 
 struct DrawConfig {
     rectangles: bool,
     wires: bool,
+    light: bool,
     frags: FragLevel,
     num_triangles: i32,
 }
@@ -47,17 +52,21 @@ struct DrawConfig {
 struct Globals {
     last_tick_time: std::sync::Arc<std::sync::Mutex<u64>>,
     context: web_sys::WebGl2RenderingContext,
+    camera_matrix_location: web_sys::WebGlUniformLocation,
     model_matrix_location: web_sys::WebGlUniformLocation,
+    light_pos_location: web_sys::WebGlUniformLocation,
     frag_enum_location: web_sys::WebGlUniformLocation,
     mouse: std::sync::Arc<std::sync::Mutex<MouseState>>,
-    camera_matrix: std::sync::Arc<std::sync::Mutex<cgmath::Matrix4<f32>>>,
+    camera_rotate_matrix: std::sync::Arc<std::sync::Mutex<cgmath::Matrix4<f32>>>,
+    camera_offset_matrix: cgmath::Matrix4<f32>,
 }
 
 fn make_globals(
     context: web_sys::WebGl2RenderingContext,
     program: web_sys::WebGlProgram,
 ) -> Globals {
-    let camera_matrix = std::sync::Arc::new(std::sync::Mutex::new(cgmath::Matrix4::identity()));
+    let camera_rotate_matrix =
+        std::sync::Arc::new(std::sync::Mutex::new(cgmath::Matrix4::identity()));
     let perspective_matrix: cgmath::Matrix4<f32> = cgmath::Matrix4::from(cgmath::PerspectiveFov {
         fovy: cgmath::Rad(2.0),
         aspect: 1.0,
@@ -75,7 +84,11 @@ fn make_globals(
     let model_matrix_location = context
         .get_uniform_location(&program, "modelMatrix")
         .unwrap();
+    let camera_matrix_location = context
+        .get_uniform_location(&program, "cameraMatrix")
+        .unwrap();
     let frag_enum_location = context.get_uniform_location(&program, "fragEnum").unwrap();
+    let light_pos_location = context.get_uniform_location(&program, "lightPos").unwrap();
     let mouse = std::sync::Arc::new(std::sync::Mutex::new(MouseState {
         mouse_down: false,
         mouse_drag_pos_prev: Some((0, 0)),
@@ -85,24 +98,77 @@ fn make_globals(
         context,
         last_tick_time: std::sync::Arc::new(std::sync::Mutex::new(0)),
         model_matrix_location,
+        camera_matrix_location,
         frag_enum_location,
+        light_pos_location,
         mouse,
-        camera_matrix,
+        camera_rotate_matrix,
+        camera_offset_matrix: cgmath::Matrix4::from_translation(cgmath::Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: -3.0,
+        }),
     }
 }
 #[wasm_bindgen]
 pub fn andy_main() {
-    set_canvas("big_canvas", 0, true, true, FragLevel::Transparent);
-    set_canvas("small_canvas0", 0, true, false, FragLevel::None);
-    set_canvas("small_canvas1", 0, true, true, FragLevel::None);
-    set_canvas("small_canvas2", 0, true, true, FragLevel::Transparent);
-    set_canvas("small_canvas3", 1, true, true, FragLevel::Transparent);
-    set_canvas("small_canvas4", 2, true, true, FragLevel::Transparent);
-    set_canvas("small_canvas5", 3, true, true, FragLevel::Transparent);
-    set_canvas("small_canvas6", 3, false, true, FragLevel::Full);
+    set_canvas("big_canvas", 0, true, true, false, FragLevel::Transparent);
+    set_canvas("small_canvas0", 0, true, false, false, FragLevel::None);
+    set_canvas("small_canvas1", 0, true, true, false, FragLevel::None);
+    set_canvas(
+        "small_canvas2",
+        0,
+        true,
+        true,
+        false,
+        FragLevel::Transparent,
+    );
+    set_canvas(
+        "small_canvas3",
+        1,
+        true,
+        true,
+        false,
+        FragLevel::Transparent,
+    );
+    set_canvas(
+        "small_canvas4",
+        2,
+        true,
+        true,
+        false,
+        FragLevel::Transparent,
+    );
+    set_canvas(
+        "small_canvas5",
+        3,
+        true,
+        true,
+        false,
+        FragLevel::Transparent,
+    );
+    set_canvas("small_canvas6", 3, false, true, false, FragLevel::Full);
+    set_canvas("small_canvas7", 3, false, true, true, FragLevel::Reflective);
+    set_canvas("small_canvas8", 3, false, true, true, FragLevel::Spectral);
+    set_canvas("small_canvas9", 3, false, false, true, FragLevel::Spectral);
+    set_canvas(
+        "small_canvas10",
+        3,
+        false,
+        false,
+        true,
+        FragLevel::BeachBall,
+    );
 }
 
-fn set_canvas(name: &str, iters: u32, rectangles: bool, wires: bool, frags: FragLevel) {
+fn set_canvas(
+    name: &str,
+    iters: u32,
+    rectangles: bool,
+    wires: bool,
+    light: bool,
+    frags: FragLevel,
+) {
     let mut verts: Vec<f32> = GOLDEN_RECTANGLE_VERTS.to_vec();
     let mut ico_verts = icosahedron::generate_verticies();
     for _ in 0..iters {
@@ -126,6 +192,7 @@ fn set_canvas(name: &str, iters: u32, rectangles: bool, wires: bool, frags: Frag
                 draw(
                     globals,
                     DrawConfig {
+                        light,
                         rectangles,
                         wires,
                         frags,
@@ -179,7 +246,7 @@ fn draw(globals: Globals, config: DrawConfig) {
             None
         }
     } {
-        let mut camera_matrix = globals.camera_matrix.lock().unwrap();
+        let mut camera_rotate_matrix = globals.camera_rotate_matrix.lock().unwrap();
         let move_direction_vector = cgmath::Vector4 {
             x: vx as f32,
             y: vy as f32,
@@ -196,21 +263,22 @@ fn draw(globals: Globals, config: DrawConfig) {
             .truncate()
             .cross(move_direction_vector.truncate());
         if length(cross) != 0.0 {
-            *camera_matrix = cgmath::Matrix4::from_axis_angle(
+            *camera_rotate_matrix = cgmath::Matrix4::from_axis_angle(
                 normalize(cross),
                 cgmath::Rad(length(cross) / 200.0),
-            ) * (*camera_matrix);
+            ) * (*camera_rotate_matrix);
         }
+        globals.context.uniform_matrix4fv_with_f32_array(
+            Some(&globals.camera_matrix_location),
+            false,
+            &matrix_to_vec(globals.camera_offset_matrix * *camera_rotate_matrix),
+        );
     }
 
-    let forward_rotated = cgmath::Matrix4::from_translation(cgmath::Vector3 {
-        x: 0.0,
-        y: 0.0,
-        z: -3.0,
-    }) * *globals.camera_matrix.lock().unwrap();
+    let model_matrix = cgmath::Matrix4::identity();
 
     if config.rectangles {
-        draw_rectangle(&globals, forward_rotated, FragEnum::Red);
+        draw_rectangle(&globals, model_matrix, FragEnum::Red);
         let rotated_1 = cgmath::Matrix4::from_axis_angle(
             normalize(cgmath::Vector3 {
                 x: 1.0,
@@ -226,9 +294,9 @@ fn draw(globals: Globals, config: DrawConfig) {
             }),
             cgmath::Rad(std::f32::consts::TAU / 4.0),
         );
-        draw_rectangle(&globals, forward_rotated * rotated_1, FragEnum::Green);
+        draw_rectangle(&globals, model_matrix * rotated_1, FragEnum::Green);
 
-        draw_rectangle(&globals, forward_rotated, FragEnum::Red);
+        draw_rectangle(&globals, model_matrix, FragEnum::Red);
         let rotated_2 = cgmath::Matrix4::from_axis_angle(
             normalize(cgmath::Vector3 {
                 x: 1.0,
@@ -244,17 +312,53 @@ fn draw(globals: Globals, config: DrawConfig) {
             }),
             cgmath::Rad(std::f32::consts::TAU / 4.0),
         );
-        draw_rectangle(&globals, forward_rotated * rotated_2, FragEnum::Blue);
+        draw_rectangle(&globals, model_matrix * rotated_2, FragEnum::Blue);
+    }
+
+    if config.light {
+        let light_pos = (cgmath::Matrix4::from_axis_angle(
+            normalize(cgmath::Vector3 {
+                x: 0.0,
+                y: 0.0,
+                z: 1.0,
+            }),
+            cgmath::Rad((((current_time_milis % 3000) as f32) / 3000.0) * std::f32::consts::TAU),
+        ) * cgmath::Vector4 {
+            x: 2.5,
+            y: 0.0,
+            z: 0.0,
+            w: 1.0,
+        })
+        .truncate();
+        globals.context.uniform3f(
+            Some(&globals.light_pos_location),
+            light_pos.x,
+            light_pos.y,
+            light_pos.z,
+        );
+
+        let poo = cgmath::Matrix4::from_translation(light_pos) * cgmath::Matrix4::from_scale(0.2);
+        draw_triangles_frags(
+            &globals,
+            poo,
+            Some(FragEnum::White),
+            6,
+            config.num_triangles,
+            false,
+        );
     }
 
     let bigger = cgmath::Matrix4::from_scale(((PHI * PHI) + (1.0 * 1.0)).sqrt());
     draw_triangles_frags(
         &globals,
-        forward_rotated * bigger,
+        model_matrix * bigger,
         match config.frags {
             FragLevel::Transparent => Some(FragEnum::ClearRed),
             FragLevel::None => None,
             FragLevel::Full => Some(FragEnum::Red),
+            FragLevel::Reflective => Some(FragEnum::RedReflective),
+            FragLevel::Spectral => Some(FragEnum::RedSpectral),
+            FragLevel::BeachBall => Some(FragEnum::BeachBall),
         },
         6,
         config.num_triangles,
--- crates/icosahedron/src/shaders_source/vertex.glsl
+++ crates/icosahedron/src/shaders_source/vertex.glsl
@@ -1,13 +1,13 @@
 #version 300 es
 
 uniform mat4 perspectiveMatrix;
+uniform mat4 cameraMatrix;
 uniform mat4 modelMatrix;
 
 in vec3 position;
-out vec4 input_pos;
+out vec4 inputPos;
 
 void main() {
-    input_pos = vec4(position, 1.0);
-
-    gl_Position = perspectiveMatrix * modelMatrix * vec4(position, 1.0);
+    inputPos = modelMatrix * vec4(position, 1.0);
+    gl_Position = perspectiveMatrix * cameraMatrix * modelMatrix * vec4(position, 1.0);
 }
