4b31ddf0b7a88475f1e6ee85179afcf89750f003
陈功 <chengong456@qq.com>
2023-12-19 23:45:27 -0800

    junk

--- root_server/main.js
+++
@@ -1,8 +0,0 @@
-import init from "./andy_wasm/icosahedron.js";
-import {andy_main, set_panic_hook} from "./andy_wasm/icosahedron.js";
-
-await init().then(() => {
-    set_panic_hook();
-});
-
-andy_main();
